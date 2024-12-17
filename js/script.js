7edecaddbc6033c1df325cfebf430cda2948f9bc
JoaoDelbin <joao.delbin17@gmail.com>
2024-12-17 19:30:36 -0300

    finalizando raiz

--- js/script.js
+++ js/script.js
@@ -23,3 +23,31 @@ function vermais() {
     btnvermais.innerHTML = 'Leia Menos';
   }
 }
+
+// 
+// 
+// 
+
+function playVideo3() {
+  const iframe = document.getElementById('video3');
+  const videoUrl = "https://www.youtube.com/embed/NV4TId1dDec?autoplay=1";
+  iframe.src = videoUrl;
+  iframe.style.display = 'block'; 
+  document.querySelector('.video-thumbnail3').style.display = 'none'; 
+}
+
+function playVideo4() {
+  const iframe = document.getElementById('video4');
+  const videoUrl = "https://www.youtube.com/embed/gP2gc39oPwM?autoplay=1";
+  iframe.src = videoUrl;
+  iframe.style.display = 'block'; 
+  document.querySelector('.video-thumbnail4').style.display = 'none'; 
+}
+
+function playVideo5() {
+  const iframe = document.getElementById('video5');
+  const videoUrl = "https://www.youtube.com/embed/jfA-ZXq7RpM?autoplay=1";
+  iframe.src = videoUrl;
+  iframe.style.display = 'block'; 
+  document.querySelector('.video-thumbnail5').style.display = 'none'; 
+}

b1f1362c3b08aad0ca016e657cad7b0c7ba383ea
JoaoDelbin <joao.delbin17@gmail.com>
2025-01-13 17:07:10 -0300

    attvideo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function playVideo() {
   const iframe = document.getElementById('video');
-  const videoUrl = "https://www.youtube.com/embed/2-6pLqW8QGU?autoplay=1";
+  const videoUrl = "https://www.youtube.com/embed/rJqPObtAHzs?autoplay=1";
   iframe.src = videoUrl;
   iframe.style.display = 'block'; 
   document.querySelector('.video-thumbnail').style.display = 'none'; 
